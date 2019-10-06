224333f3dd5bd255c14017ed1fb678a5a97c1e39
Ll0k0s <Ll0k0s.ukr.net>
2019-10-06 19:32:06 +0300

    +updates

---
+++ kpi/dk62_sokol/lab3_kernel_threads/src/firstmod.mod.c
@@ -0,0 +1,47 @@
+#include <linux/build-salt.h>
+#include <linux/module.h>
+#include <linux/vermagic.h>
+#include <linux/compiler.h>
+
+BUILD_SALT;
+
+MODULE_INFO(vermagic, VERMAGIC_STRING);
+MODULE_INFO(name, KBUILD_MODNAME);
+
+__visible struct module __this_module
+__attribute__((section(".gnu.linkonce.this_module"))) = {
+	.name = KBUILD_MODNAME,
+	.init = init_module,
+#ifdef CONFIG_MODULE_UNLOAD
+	.exit = cleanup_module,
+#endif
+	.arch = MODULE_ARCH_INIT,
+};
+
+#ifdef CONFIG_RETPOLINE
+MODULE_INFO(retpoline, "Y");
+#endif
+
+static const struct modversion_info ____versions[]
+__used
+__attribute__((section("__versions"))) = {
+	{ 0x9cbc70c9, "module_layout" },
+	{ 0xab9e5ae3, "param_ops_int" },
+	{ 0x25597c98, "wake_up_process" },
+	{ 0xac84e8ce, "kthread_create_on_node" },
+	{ 0xd2b09ce5, "__kmalloc" },
+	{ 0x37a0cba, "kfree" },
+	{ 0x7c32d0f0, "printk" },
+	{ 0x6202efec, "kmem_cache_alloc_trace" },
+	{ 0xd1e83caf, "kmalloc_caches" },
+	{ 0x1000e51, "schedule" },
+	{ 0xbdfb6dbb, "__fentry__" },
+};
+
+static const char __module_depends[]
+__used
+__attribute__((section(".modinfo"))) =
+"depends=";
+
+
+MODULE_INFO(srcversion, "F8459CBA7054564B93E45E4");
